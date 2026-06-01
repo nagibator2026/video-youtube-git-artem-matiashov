a7dacca061117ff3a87ef0f4046c9cb4856a85fd
Senior Man <manJs@mail.com>
2026-06-01 11:08:32 +0300

    Init commit

---
+++ app.py
@@ -0,0 +1,14 @@
+ Строка (String)
+name = "Алексей"
+print(name) 
+
+# Числа (целые и с плавающей точкой)
+age = 25
+temperature = 36.6
+print(age)
+print(temperature)
+
+# Логический тип (Boolean). Обратите внимание на заглавные буквы!
+is_active = True 
+is_logged_in = False
+print(is_active)
